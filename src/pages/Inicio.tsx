9ec05493842ae0bdde4bbd26658e8cd78f4feeba
Maria Fernanda Pineda Olivas <est.mpineda@uml.edu.ni>
2023-08-05 12:35:22 -0600

    inclusion de appi

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,14 @@
 import { Title } from '../components/Title';
+import { useEffect } from 'react'
 
 export const Inicio = () => {
+
+  useEffect(() => {
+    fetch('https:jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    
+  });
   return (
     <>
       <Title titulo="Bienvenido!" />
